3e8e7a3862527de62a645b5148717bf2355e20ba
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-23 17:33:21 +0100

    Commit initial : Création du projet Spring Boot avec les dépendances et la structure MVC

---
+++ src/main/java/com/example/ENAA__Fil_Rouge/EnaaFilRougeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ENAA__Fil_Rouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaaFilRougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnaaFilRougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ENAA__Fil_Rouge/EnaaFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ENAA__Fil_Rouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnaaFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
