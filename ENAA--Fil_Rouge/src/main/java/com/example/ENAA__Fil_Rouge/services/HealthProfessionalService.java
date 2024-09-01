fa2129596fec755fd20341b8c1a2ade897506fae
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-02 00:00:55 +0100

    j'ai cree des fonctions dans service pour Healthprof

--- ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/services/HealthProfessionalService.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/services/HealthProfessionalService.java
@@ -1,19 +1,43 @@
 package com.example.ENAA__Fil_Rouge.services;
 
-
+import com.example.ENAA__Fil_Rouge.models.HealthProfessional;
+import com.example.ENAA__Fil_Rouge.repositories.HealthProfessionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HealthProfessionalService {
 
+    @Autowired
+    private HealthProfessionalRepository healthProfessionalRepository;
 
+    public HealthProfessional registerHealthProfessional(HealthProfessional healthProfessional) {
+        return healthProfessionalRepository.save(healthProfessional);
+    }
 
-//    public void exampleMethod() {
-//        HealthProfessional he = new HealthProfessional();
-//
-//        he.setUsername("Dr. John Doe");
-////        String name = he.getName();
-//
-//        doctor.save(he);
-//    }
+    public List<HealthProfessional> getAllHealthProfessionals() {
+        return healthProfessionalRepository.findAll();
+    }
+
+    public HealthProfessional showHealthProfessionalById(Long id) {
+        return healthProfessionalRepository.findById(id)
+                .orElseThrow();
+    }
+    public HealthProfessional updateHealthProfessional(Long id, HealthProfessional updatedHealthProfessional) {
+        HealthProfessional healthProfessional = showHealthProfessionalById(id);
+        healthProfessional.setUsername(updatedHealthProfessional.getUsername());
+        healthProfessional.setEmail(updatedHealthProfessional.getEmail());
+        healthProfessional.setPassword(updatedHealthProfessional.getPassword());
+        healthProfessional.setPhoneNumber(updatedHealthProfessional.getPhoneNumber());
+        healthProfessional.setClinicAddress(updatedHealthProfessional.getClinicAddress());
+        healthProfessional.setSpecialty(updatedHealthProfessional.getSpecialty());
+        healthProfessional.setRegistrationNumber(updatedHealthProfessional.getRegistrationNumber());
+        return healthProfessionalRepository.save(healthProfessional);
+    }
+
+    public void deleteHealthProfessional(Long id) {
+        healthProfessionalRepository.deleteById(id);
+    }
 }
