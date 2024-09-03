9c249f8f481258d4c548b7723547c9fce25df870
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-03 23:56:47 +0100

    j'ai cree unelogique de Notification par email

--- ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/repositories/AppointmentRepository.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/repositories/AppointmentRepository.java
@@ -4,9 +4,9 @@ import com.example.ENAA__Fil_Rouge.models.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDateTimeBeforeAndNotificationEnvoyeeFalse(LocalDateTime dateTime);
 
 }
--- ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/services/NotificationService.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/services/NotificationService.java
@@ -20,4 +20,47 @@ public class NotificationService {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+
+    @Scheduled(fixedRate = 3600000)
+    public void envoyerNotifications() {
+        LocalDateTime maintenant = LocalDateTime.now();
+
+        LocalDateTime dans24Heures = maintenant.plusHours(24);
+
+        List<Appointment> appointments = appointmentRepository
+                .findByDateTimeBeforeAndNotificationEnvoyeeFalse(dans24Heures);
+
+
+        for (Appointment appointment : appointments) {
+            String nomPatient = appointment.getPatient().getUsername();
+            String nomProfessional = appointment.getProfessional().getUsername();
+            envoyerEmail(appointment.getPatient().getEmail(), nomPatient, appointment.getDateTime(), nomProfessional);
+            appointment.setNotificationEnvoyee(true);
+            appointmentRepository.save(appointment);
+        }
+    }
+
+
+    private void envoyerEmail(String emailPatient, String nomPatient, LocalDateTime dateTime, String nomProfessional) {
+        String message = "Bonjour " + nomPatient + ",\n\n" +
+                "Ceci est un rappel pour votre rendez-vous prévu le " + dateTime + ".\n" +
+                "Votre rendez-vous est avec le Dr. " + nomProfessional + ".\n\n" +
+                "Merci de votre attention.";
+
+
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("doctNet@outlook.com");
+        mailMessage.setTo(emailPatient);
+        mailMessage.setSubject("Rappel de votre rendez-vous");
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
 }
