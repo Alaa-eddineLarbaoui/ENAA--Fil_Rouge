cb92520e5388408fc5ef311dee0629f917d12498
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-03 23:57:53 +0100

    Ajout de AppointmentController pour gérer les rendez-vous

---
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/controllers/AppointmentController.java
@@ -0,0 +1,41 @@
+package com.example.ENAA__Fil_Rouge.controllers;
+
+import com.example.ENAA__Fil_Rouge.models.Appointment;
+import com.example.ENAA__Fil_Rouge.services.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/appointments")
+public class AppointmentController {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping("/add")
+    public Appointment addAppointment(@RequestBody Appointment appointment) {
+        return appointmentService.addAppointment(appointment);
+    }
+
+    @GetMapping("/getAll")
+    public List<Appointment> getAllAppointments() {
+        return appointmentService.getAllAppointment();
+    }
+
+    @GetMapping("/get/{id}")
+    public Appointment getAppointmentById(@PathVariable Long id) {
+        return appointmentService.getAppointment(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteAppointment(@PathVariable Long id) {
+        appointmentService.delete(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Appointment updateAppointment(@PathVariable Long id, @RequestBody Appointment appointment) {
+        return appointmentService.updateAppointment(appointment, id);
+    }
+}
