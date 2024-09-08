730dd55b6f1a0cfcc3daa2d6a06fc932f188bdda
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-09 00:31:27 +0100

    J'ai cree une logique pour Récupèrer les créneaux horaires disponibles pour un médecin donné à une date

---
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/services/AvailabilityService.java
@@ -0,0 +1,52 @@
+package com.example.ENAA__Fil_Rouge.services;
+
+import com.example.ENAA__Fil_Rouge.enums.AppointmentReason;
+import com.example.ENAA__Fil_Rouge.enums.AppointmentStatus;
+import com.example.ENAA__Fil_Rouge.models.Appointment;
+import com.example.ENAA__Fil_Rouge.models.Availability;
+import com.example.ENAA__Fil_Rouge.models.HealthProfessional;
+import com.example.ENAA__Fil_Rouge.models.Patient;
+import com.example.ENAA__Fil_Rouge.repositories.AppointmentRepository;
+import com.example.ENAA__Fil_Rouge.repositories.AvailabilityRepository;
+import com.example.ENAA__Fil_Rouge.repositories.HealthProfessionalRepository;
+import com.example.ENAA__Fil_Rouge.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AvailabilityService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
+    private HealthProfessionalRepository professionalRepository;
+
+    @Autowired
+    private AvailabilityRepository availabilityRepository;
+
+
+    public List<LocalTime> getAvailableTimes(LocalDate date, Long professionalId) {
+        List<Appointment> reservedAppointments = appointmentRepository.findByDateAndProfessionalId(date, professionalId);
+        List<LocalTime> reservedTimes = reservedAppointments.stream()
+                .map(Appointment::getTime)
+                .collect(Collectors.toList());
+
+        List<Availability> availabilities = availabilityRepository.findByDateAndProfessionalId(date, professionalId);
+
+        // Filtrer les créneaux disponibles en fonction des réservations déjà effectuées
+        return availabilities.stream()
+                .filter(availability -> !reservedTimes.contains(availability.getStartTime()))
+                .map(Availability::getStartTime)
+                .collect(Collectors.toList());
+    }
+
+}
