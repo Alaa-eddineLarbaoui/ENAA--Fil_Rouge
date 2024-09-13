b6cb3fcc5842abd28498d98deffe1418f1398f77
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-13 18:05:17 +0100

    j'ai cree filter pour la securite

--- ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/models/Person.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/ENAA__Fil_Rouge/models/Person.java
@@ -1,8 +1,14 @@
 package com.example.ENAA__Fil_Rouge.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.example.ENAA__Fil_Rouge.enums.Erole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,7 +16,7 @@ import lombok.*;
 @AllArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Person {
+public abstract class Person implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +32,46 @@ public abstract class Person {
 
     @Column(length = 15, unique = true, nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false)
+    private Erole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == null) {
+
+            System.out.println("Role is not initialized.");
+            return List.of();
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
