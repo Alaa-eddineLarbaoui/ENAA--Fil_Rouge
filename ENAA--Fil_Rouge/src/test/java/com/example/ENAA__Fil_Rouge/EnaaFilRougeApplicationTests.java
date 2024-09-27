c54e51c093be5d39228aadab2769a3e0978f8991
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-09-27 23:44:41 +0100

    j'ai ajoute docker file et docker compose dans  le projet

--- ENAA--Fil_Rouge/src/test/java/com/example/ENAA__Fil_Rouge/EnaaFilRougeApplicationTests.java
+++ ENAA--Fil_Rouge/src/test/java/com/example/ENAA__Fil_Rouge/EnaaFilRougeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.ENAA__Fil_Rouge;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EnaaFilRougeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.ENAA__Fil_Rouge;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EnaaFilRougeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
