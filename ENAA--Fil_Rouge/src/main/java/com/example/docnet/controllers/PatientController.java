ca21779fc73e89b1220fcf4c500e57ee9f0a48b2
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-10-21 23:53:51 +0100

    j'ai structure le code

--- ENAA--Fil_Rouge/src/main/java/com/example/docnet/controllers/PatientController.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/docnet/controllers/PatientController.java
@@ -13,30 +13,60 @@ public class PatientController {
 
     private final PatientService patientService;
 
+    // Constructeur injectant le service PatientService.
     public PatientController(PatientService patientService) {
         this.patientService = patientService;
     }
 
+    /**
+     * Récupère tous les patients.
+     *
+     * @return La liste de tous les patients.
+     */
     @GetMapping("/getAll")
     public List<Patient> showAllPatients() {
         return patientService.getAllPatients();
     }
 
+    /**
+     * Récupère un patient par son ID.
+     *
+     * @param id L'ID du patient à récupérer.
+     * @return Le patient correspondant à l'ID.
+     */
     @GetMapping("/get/{id}")
     public Patient getPatientById(@PathVariable Long id) {
         return patientService.showPatientById(id);
     }
 
+    /**
+     * Enregistre un nouveau patient.
+     *
+     * @param patient Les informations du patient à enregistrer.
+     * @return Le patient enregistré.
+     */
     @PostMapping("/register")
     public Patient registerPatient(@RequestBody Patient patient) {
         return patientService.registerPatient(patient);
     }
 
+    /**
+     * Met à jour les informations d'un patient existant.
+     *
+     * @param id L'ID du patient à mettre à jour.
+     * @param updatedPatient Les nouvelles informations du patient.
+     * @return Le patient mis à jour.
+     */
     @PutMapping("update/{id}")
     public Patient updatePatient(@PathVariable Long id, @RequestBody Patient updatedPatient) {
         return patientService.updatePatient(id, updatedPatient);
     }
 
+    /**
+     * Supprime un patient par son ID.
+     *
+     * @param id L'ID du patient à supprimer.
+     */
     @DeleteMapping("delete/{id}")
     public void deletePatient(@PathVariable Long id) {
         patientService.deletePatient(id);
