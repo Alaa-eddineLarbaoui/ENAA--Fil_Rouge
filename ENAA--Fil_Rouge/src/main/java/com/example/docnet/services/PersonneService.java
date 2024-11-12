8517157c6c0313f1b9b56c7c69e0c0d097d9fd90
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-11-12 23:54:13 +0100

    l'ajout des changements

--- ENAA--Fil_Rouge/src/main/java/com/example/docnet/services/PersonneService.java
+++ ENAA--Fil_Rouge/src/main/java/com/example/docnet/services/PersonneService.java
@@ -19,11 +19,24 @@ import java.util.Map;
 @Service
 @AllArgsConstructor
 public class PersonneService {
+
+    /**
+     * Data repository for Person entities.
+     */
     @Autowired
     private PersoneRepository personeRepository;
 
-
+    /**
+     * Password encoder to secure login information.
+     */
     private final PasswordEncoder passwordEncoder;
+
+    /**
+     * Registers a new person in the database based on their role.
+     * @param request Object containing the person's signup information.
+     * @return A JSON message indicating that signup was successful.
+     * @throws JsonProcessingException If an error occurs during JSON conversion.
+     */
     public String register(SingUpDto request) throws JsonProcessingException {
         Person person = createUserByRole(request);
         personeRepository.save(person);
@@ -31,10 +44,15 @@ public class PersonneService {
         Map<String, String> response = new HashMap<>();
         response.put("message", "person signup");
 
-
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(response);
     }
+
+    /**
+     * Creates a Person instance based on the role specified in the SingUpDto object.
+     * @param singUpDto Object containing signup information.
+     * @return A Person instance with the defined information.
+     */
     private Person createUserByRole(SingUpDto singUpDto) {
         String encodedPassword = passwordEncoder.encode(singUpDto.getPassword());
 
@@ -46,34 +64,42 @@ public class PersonneService {
         person.setUsername(singUpDto.getUsername());
         person.setEmail(singUpDto.getEmail());
         person.setPassword(encodedPassword);
-        System.out.println(person.getRole()+"jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj");
+        System.out.println(person.getRole() + " - Role assigned to the user.");
         person.setRole(singUpDto.getRole());
         return person;
     }
 
-
+    /**
+     * Registers a new doctor in the database.
+     * @param request Object containing the doctor's signup information.
+     * @return A JSON message indicating that signup was successful.
+     * @throws JsonProcessingException If an error occurs during JSON conversion.
+     */
     public String registerDoctor(Signupdoctor request) throws JsonProcessingException {
+        Person person = createUserByRolee(request);
+        personeRepository.save(person);
 
-         Person person = createUserByRolee(request);
-
-         personeRepository.save(person);
-
-         Map<String, String> response = new HashMap<>();
+        Map<String, String> response = new HashMap<>();
         response.put("message", "person signup");
 
-         ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(response);
     }
 
+    /**
+     * Creates a Person instance based on the role specified in the Signupdoctor object.
+     * @param signupdoctor Object containing the doctor's signup information.
+     * @return A Person instance with the defined information.
+     */
     private Person createUserByRolee(Signupdoctor signupdoctor) {
-         String encodedPassword = passwordEncoder.encode(signupdoctor.getPassword());
+        String encodedPassword = passwordEncoder.encode(signupdoctor.getPassword());
 
-         Person person = switch (signupdoctor.getRole()) {
+        Person person = switch (signupdoctor.getRole()) {
             case DOCTOR -> new HealthProfessional();
             default -> new Patient();
         };
 
-         person.setUsername(signupdoctor.getUsername());
+        person.setUsername(signupdoctor.getUsername());
         person.setEmail(signupdoctor.getEmail());
         person.setPassword(encodedPassword);
         person.setRole(signupdoctor.getRole());
@@ -82,16 +108,4 @@ public class PersonneService {
 
         return person;
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
